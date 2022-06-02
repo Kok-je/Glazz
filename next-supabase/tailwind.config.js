3c6cdd711342968476302b8b9a30ef5a0f8f69ce
Shawn Kok <shawnkok00@gmail.com>
2022-06-02 18:09:28 +0800

    updated to make tailwind work

--- next-supabase/tailwind.config.js
+++ next-supabase/tailwind.config.js
@@ -1,7 +1,36 @@
 module.exports = {
-  content: [],
+  content: ['./pages/**/*.{html,js}'],
   theme: {
-    extend: {},
+    theme: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      colors: {
+        'blue': '#1fb6ff',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      extend: {
+        spacing: {
+          '128': '32rem',
+          '144': '36rem',
+        },
+        borderRadius: {
+          '4xl': '2rem',
+        }
+      }
+    }
   },
   plugins: [require('@tailwindcss/typography')],
 }
